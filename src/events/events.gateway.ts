596799f23cb9838f1b2e9802070a961e52958693
김범진 <sdpthf@naver.com>
2022-01-29 17:37:31 +0900

    📑 :: EventGateway

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,40 @@
+import {
+    ConnectedSocket, MessageBody,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    OnGatewayInit, SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer
+} from "@nestjs/websockets";
+import {Server, Socket} from 'socket.io';
+import {Logger} from "@nestjs/common";
+
+@WebSocketGateway(81, {namespace: 'chat'})
+export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+    @WebSocketServer() server: Server;
+
+    private logger: Logger = new Logger('Gateway');
+
+    @SubscribeMessage('events')
+    handleEvent(@MessageBody() data: string): string {
+        return data;
+    }
+
+    @SubscribeMessage('messages')
+    handleMessage(client: Socket, data: any): void {
+        client.emit('message', data);
+    }
+
+    afterInit(server: Server): any {
+        this.logger.log('init');
+    }
+
+    handleConnection(@ConnectedSocket() socket: Socket) {
+        this.logger.log('connected', socket.nsp.name);
+    }
+
+    handleDisconnect(@ConnectedSocket() socket: Socket) {
+        this.logger.log('disconnected', socket.nsp.name);
+    }
+
+}
