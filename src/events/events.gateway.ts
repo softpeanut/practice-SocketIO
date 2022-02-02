120df4896b86b263465d2b3d92719893919df012
김범진 <sdpthf@naver.com>
2022-02-02 14:19:33 +0900

    ⚡ :: 코드 수정

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -9,34 +9,33 @@ import {
 import {Server, Socket} from 'socket.io';
 import {Logger} from "@nestjs/common";
 
-@WebSocketGateway(81, {namespace: 'chat'})
+@WebSocketGateway(81, {namespace: 'chats'})
 export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer() server: Server;
 
-    private logger: Logger = new Logger('Gateway');
-
-    @SubscribeMessage('events')
-    handleEvent(@MessageBody() data: string): string {
-        this.logger.log('handle event');
-        return data;
-    }
+    private logger: Logger = new Logger('chats');
 
     @SubscribeMessage('messages')
-    handleMessage(client: Socket, data: any): void {
+    handleMessage(@MessageBody() data: string,
+                  @ConnectedSocket() client: Socket,) {
         this.logger.log('handle message');
-        client.emit('message', data);
+        this.logger.log(data.valueOf());
+        client.broadcast.emit('new_message', {
+            data,
+            client: client.id,
+        });
     }
 
-    afterInit(server: Server): any {
+    afterInit(server: Server) {
         this.logger.log('init');
     }
 
     handleConnection(@ConnectedSocket() socket: Socket) {
-        this.logger.log('connected', socket.nsp.name);
+        this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`);
     }
 
     handleDisconnect(@ConnectedSocket() socket: Socket) {
-        this.logger.log('disconnected', socket.nsp.name);
+        this.logger.log(`disconnected : ${socket.id} ${socket.nsp.name}`);
     }
 
 }
