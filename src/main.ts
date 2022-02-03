4c27d21efe0bed154cf09756776ed24494f1d637
김범진 <sdpthf@naver.com>
2022-02-03 11:22:30 +0900

    ⚡ :: cors 설정

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   console.log('server port 3000')
   await app.listen(3000);
 }
